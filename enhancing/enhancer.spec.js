3f19b82e4f1371b9afab481a2d7b5ede13b9583c
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-08 15:47:39 -0400

    added tests for enhancement fail

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -27,6 +27,33 @@ describe('enhancer.js', () => {
                 expect(enhancer.repair(item).durability).toBe(100);
                 expect(enhancer.repair(item).enhancement).toBe(item.enhancement);
             });
+
+            it('should not change the name of an item', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.repair(item).name).toBe(item.name);
+            });
+
+            it('should not change the enhancement of an item', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.repair(item).enhancement).toBe(item.enhancement);
+            });
+
+            it('should change the durability of an item to 100', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.repair(item).durability).toBe(100);
+            });
         });
 
         describe('succeed(item)', () => {
@@ -36,6 +63,8 @@ describe('enhancer.js', () => {
                     durability: 0,
                     enhancement: 0
                 }
+                expect(enhancer.succeed(item).name).toBe(item.name);
+                expect(enhancer.succeed(item).durability).toBe(item.durability);
                 expect(enhancer.succeed(item).enhancement).toBe(item.enhancement+1);
             });
 
@@ -45,7 +74,27 @@ describe('enhancer.js', () => {
                     durability: 0,
                     enhancement: 20
                 }
+                expect(enhancer.succeed(item).name).toBe(item.name);
+                expect(enhancer.succeed(item).durability).toBe(item.durability);
                 expect(enhancer.succeed(item).enhancement).toBe(20);
+            });
+
+            it('should enhance an item', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.succeed(item).enhancement).toBe(item.enhancement+1);
+            });
+
+            it('should not change the item name', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.succeed(item).enhancement).toBe(item.enhancement+1);
             });
 
             it('should not change the durability', () => {
@@ -59,8 +108,86 @@ describe('enhancer.js', () => {
         });
 
         describe('fail(item)', () => {
-            it('should return an enhanced object', () => {
-                
+            it('should return an enhanced object at level 0', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.fail(item).name).toBe(item.name);
+                expect(enhancer.fail(item).durability).toBe(item.durability);
+                expect(enhancer.fail(item).enhancement).toBe(item.enhancement-5);
+            });
+
+            it('should return an enhanced object at level 14', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 14
+                }
+                expect(enhancer.fail(item).name).toBe(item.name);
+                expect(enhancer.fail(item).durability).toBe(item.durability);
+                expect(enhancer.fail(item).enhancement).toBe(item.enhancement-5);
+            });
+
+            it('should return an enhanced object at level 15', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 15
+                }
+                expect(enhancer.fail(item).name).toBe(item.name);
+                expect(enhancer.fail(item).durability).toBe(item.durability);
+                expect(enhancer.fail(item).enhancement).toBe(item.enhancement-10);
+            });
+
+            it('should return an enhanced object at level 16', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 16
+                }
+                expect(enhancer.fail(item).name).toBe(item.name);
+                expect(enhancer.fail(item).durability).toBe(item.durability);
+                expect(enhancer.fail(item).enhancement).toBe(item.enhancement-10);
+            });
+
+            it('should return an enhanced object at level greater than 16', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 18
+                }
+                expect(enhancer.fail(item).name).toBe(item.name);
+                expect(enhancer.fail(item).durability).toBe(item.durability);
+                expect(enhancer.fail(item).enhancement).toBe(item.enhancement-1);
+            });
+
+            it('should enhance an item at level 20', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 20
+                }
+                expect(enhancer.fail(item).enhancement).toBe(item.enhancement-1);
+            });
+
+            it('should not change the item name', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.fail(item).enhancement).toBe(item.enhancement+1);
+            });
+
+            it('should not change the durability', () => {
+                const item = {
+                    name: "name",
+                    durability: 0,
+                    enhancement: 0
+                }
+                expect(enhancer.fail(item).durability).toBe(item.durability);
             });
         });
 
